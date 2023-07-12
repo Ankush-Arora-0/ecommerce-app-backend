fb74b3d5bfbe392b9dc65b51b8a09a1037e5c471
Ankush-Arora-0 <ankushsatnam@gmail.com>
2023-07-13 00:35:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import './db/conn.js';
+import router  from './router/auth.js';
+import Razorpay from 'razorpay'
+import PaymentRouter from './router/Paymentroute.js';
+import BodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(router);
+app.use(BodyParser.json());
+app.use(express.json());
+app.use('/api',PaymentRouter)
+export const instance = new Razorpay({
+    key_id:process.env.KEY_ID,
+    key_secret:process.env.KEY_SECRET
+})
+
+app.listen(PORT,()=>{
+    console.log(`Our server is running at port ${PORT}`);
+})
